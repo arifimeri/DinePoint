8d8e145fb610f5d6a76436b9731cd387a73dbc6c
Arif Imeri <arifimeri.11@gmail.com>
2024-11-17 21:54:26 +0100

    First Commit

---
+++ src/main/java/com/example/DinePoint/DinePointApplication.java
@@ -0,0 +1,13 @@
+package com.example.DinePoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinePointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DinePoint/DinePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DinePoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
