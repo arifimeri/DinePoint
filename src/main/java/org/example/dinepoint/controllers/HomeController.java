f85c09a71b195d513a5cccfb85dd9ea45c2d76ac
Arif Imeri <arifimeri.11@gmail.com>
2025-01-28 11:51:28 +0100

    fixed Controller

--- src/main/java/org/example/dinepoint/controllers/HomeController.java
+++ src/main/java/org/example/dinepoint/controllers/HomeController.java
@@ -1,45 +1,43 @@
 package org.example.dinepoint.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(HttpServletRequest request, HttpServletResponse response) {
+    @GetMapping("/index")
+    public String home() {
         return "index";
     }
 
     @GetMapping("/about")
-    public String about(HttpServletRequest request, HttpServletResponse response) {
+    public String about() {
         return "/about";
     }
 
     @GetMapping("/service")
-    public String service(HttpServletRequest request, HttpServletResponse response) {
+    public String service() {
         return "/service";
     }
 
     @GetMapping("/menu")
-    public String menu(HttpServletRequest request, HttpServletResponse response) {
+    public String menu() {
         return "/menu";
     }
 
     @GetMapping("/team")
-    public String team(HttpServletRequest request, HttpServletResponse response) {
+    public String team() {
         return "/team";
     }
 
     @GetMapping("/testimonial")
-    public String testimonials(HttpServletRequest request, HttpServletResponse response) {
+    public String testimonials() {
         return "/testimonial";
     }
 
     @GetMapping("/contact")
-    public String contact(HttpServletRequest request, HttpServletResponse response) {
+    public String contact() {
         return "/contact";
     }
 }
